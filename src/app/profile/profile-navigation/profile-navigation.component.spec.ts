941ceb1755b4204b9363e732544889409db1b5b2
Infernum_altd <altd0606@gmail.com>
2020-04-16 13:48:11 +0300

    "add profile navbar/ left profile navbar"

--- src/app/profile/profile-navigation/profile-navigation.component.spec.ts
+++ src/app/profile/profile-navigation/profile-navigation.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CloseComponent } from './close.component';
+import { ProfileNavigationComponent } from './profile-navigation.component';
 
-describe('CloseComponent', () => {
-  let component: CloseComponent;
-  let fixture: ComponentFixture<CloseComponent>;
+describe('ProfileNavigationComponent', () => {
+  let component: ProfileNavigationComponent;
+  let fixture: ComponentFixture<ProfileNavigationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CloseComponent ]
+      declarations: [ ProfileNavigationComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CloseComponent);
+    fixture = TestBed.createComponent(ProfileNavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
