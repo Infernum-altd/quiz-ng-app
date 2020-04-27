ed89989f936b293c856395d2c46628c6420d20fc
Taya <justataya@gmail.com>
2020-04-27 03:13:37 -0700

    Add dashboard template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HTTP_INTERCEPTORS, } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, } from '@angular/common/http';
 import { QuizComponent } from './quiz/quiz.component';
 
-import {ProfileComponent} from './profile/profile.component';
-import {ProfileNavigationComponent} from './profile/profile-navigation/profile-navigation.component';
-import {LeftBarComponent} from './profile/left-bar/left-bar.component';
-import {UserInformationComponent} from './profile/user-information/user-information.component';
-import {JwtInterceptor} from './_helpers/jwt.interceptor';
-import {FriendsComponent} from './profile/friends/friends.component';
-import {MyQuizzesComponent} from './profile/my-quizzes/my-quizzes.component';
-import {FavoriteComponent} from './profile/favorite/favorite.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileNavigationComponent } from './profile/profile-navigation/profile-navigation.component';
+import { LeftBarComponent } from './profile/left-bar/left-bar.component';
+import { UserInformationComponent } from './profile/user-information/user-information.component';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { FriendsComponent } from './profile/friends/friends.component';
+import { MyQuizzesComponent } from './profile/my-quizzes/my-quizzes.component';
+import { FavoriteComponent } from './profile/favorite/favorite.component';
 import { ChangePasswordComponent } from './profile/change-password/change-password.component';
 
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -33,9 +33,18 @@ import { AnswerComponent } from './answer/answer.component';
 import { NewQuizComponent } from './new-quiz/new-quiz.component';
 import { AddQuestionsComponent } from './add-questions/add-questions.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from "@angular/material/table";
-import {MatInputModule} from "@angular/material/input";
-import {MatPaginatorModule} from "@angular/material/paginator";
+import { MatTableModule } from "@angular/material/table";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 const profileRoutes: Routes = [
   {
@@ -85,8 +94,8 @@ const appRoutes: Routes = [
     component: FriendsComponent
   },
   {
-    path:'quizzes/:id',
-    component:QuizComponent
+    path: 'quizzes/:id',
+    component: QuizComponent
   },
   {
     path: 'question',
@@ -99,6 +108,10 @@ const appRoutes: Routes = [
   {
     path: 'add_questions',
     component: AddQuestionsComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   {
     path: '',
@@ -130,7 +143,8 @@ const appRoutes: Routes = [
     ImageUploadComponent,
     AnswerComponent,
     NewQuizComponent,
-    AddQuestionsComponent
+    AddQuestionsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -142,10 +156,17 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     MatTableModule,
     MatInputModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    FlexLayoutModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
   entryComponents: [OptionalAnswerComponent, BooleanAnswerComponent, StringAnswerComponent, SequenceAnswerComponent],
